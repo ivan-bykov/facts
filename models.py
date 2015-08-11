33a9dee15f356f11c998f96ffe13852baf583939
ivan-bykov <ivan.bykov@gmail.com>
2015-08-11 17:33:23 +0400

    Updated to python 3

--- models.py
+++ models.py
@@ -8,18 +8,22 @@ class Exchange(db.Model):
     def __unicode__(self):
         return self.title
 
+    __str__ = __unicode__
+
 class Fact(db.Model):
     title = db.CharField(max_length=256)
     viewed = db.DateTimeField(auto_now_add=True)
-    tense = db.CharField(default=u'Дата', max_length=256)
-    cost = db.CharField(default=u'Стоимость', max_length=256)
-    exchange = db.CharField(default=u'Валюта', max_length=256)
-    a = db.CharField(default=u'Дополнительно А', max_length=256)
-    b = db.CharField(default=u'Дополнительно Б', max_length=256)
+    tense = db.CharField(default='Дата', max_length=256)
+    cost = db.CharField(default='Стоимость', max_length=256)
+    exchange = db.CharField(default='Валюта', max_length=256)
+    a = db.CharField(default='Дополнительно А', max_length=256)
+    b = db.CharField(default='Дополнительно Б', max_length=256)
     days = db.IntegerField(default=0)
 
     def __unicode__(self):
-        return u'%s (%s, %s)' % (self.title, self.a, self.b)
+        return '%s (%s, %s)' % (self.title, self.a, self.b)
+
+    __str__ = __unicode__
 
 class Item(db.Model):
     fact = db.ForeignKey(Fact, related_name='items')
@@ -30,6 +34,7 @@ class Item(db.Model):
     b = db.CharField(blank=True, max_length=256)
 
     def __unicode__(self):
-        return u'"%s" "%s" %s' % (self.fact.title, self.tense, self.cost)
+        return '"%s" "%s" %s' % (self.fact.title, self.tense, self.cost)
 
+    __str__ = __unicode__
     fields = tuple('tense cost exchange a b'.split())
