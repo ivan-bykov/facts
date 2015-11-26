a1caf2b222453a921dfccc3b7b2699dc132d5aa4
ivan-bykov <ivan.bykov@gmail.com>
2015-11-26 21:43:24 +0400

    delete standart comment

--- admin.py
+++ admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from facts.models import Fact, Item, Exchange
 
-# Register your models here.
 admin.site.register(Fact)
 admin.site.register(Item)
 admin.site.register(Exchange)
