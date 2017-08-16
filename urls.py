448cb1679220b10af0ac43acb0e6ba4003d5047d
ivan-bykov <ivan.bykov@gmail.com>
2017-08-16 16:42:25 +0400

    Fixed django 1.9 deprecation warning.

--- urls.py
+++ urls.py
@@ -1,9 +1,10 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from facts import views
 
-urlpatterns = patterns('',
+urlpatterns = \
+    [
     url(r'^$', views.index, name='index'),
     url(r'^days/$', views.days, name='days'),
     url(r'^days/rest/$', views.rest, name='rest'),
     url(r'^(?P<fact_id>\d+)/(?P<field>\S+)/$', views.data, name='data'),
-    )
+    ]
