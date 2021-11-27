d715e7d8e081b626f154fe71130522a400d4ac44
ivb <ivbandroid@gmail.com>
2021-11-27 18:38:25 +0400

    Upgraded to 3.2

--- urls.py
+++ urls.py
@@ -1,12 +1,12 @@
-from django.conf.urls import url
+from django.urls import re_path
 from facts import views
 
 app_name = 'facts'
 
 urlpatterns = \
     [
-    url(r'^$', views.index, name='index'),
-    url(r'^days/$', views.days, name='days'),
-    url(r'^days/rest/$', views.rest, name='rest'),
-    url(r'^(?P<fact_id>\d+)/(?P<field>\S+)/$', views.data, name='data'),
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^days/$', views.days, name='days'),
+    re_path(r'^days/rest/$', views.rest, name='rest'),
+    re_path(r'^(?P<fact_id>\d+)/(?P<field>\S+)/$', views.data, name='data'),
     ]
